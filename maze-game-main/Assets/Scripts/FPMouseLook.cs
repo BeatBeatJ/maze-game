8869c3a202ead60d8ff85edb1875c7393e6b8ebb
mariannabell <mariannabelle.doodles@gmail.com>
2021-10-21 21:33:25 -0400

    duck with movement script
    added julien's script

---
+++ maze-game-main/Assets/Scripts/FPMouseLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPMouseLook : MonoBehaviour
+{
+    public float mouseSensitivityX = 1000f;
+    public float mouseSensitivityY = 1000f;
+    public float mouseSensitivity = 1000f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
+
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
