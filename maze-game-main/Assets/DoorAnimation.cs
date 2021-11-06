edfdd39195d7158726cef2d2b63ed53070363685
BeatBeatJ <92694939+BeatBeatJ@users.noreply.github.com>
2021-11-05 20:03:44 -0400

    Added Buttons that opened Doors
    Doors confirmed. Checkmate atheists

---
+++ maze-game-main/Assets/DoorAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    private Animator Animator;
+
+    void Awake()
+    {
+       Animator = GetComponent<Animator>();
+    }
+
+    void TurnOn()
+    {
+        Animator.SetBool("ButtonPressed", true);
+    }
+}
